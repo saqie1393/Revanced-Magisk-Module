fa28fcfdb73e5efa9ae040474fbbc585fed47bf3
Lassie111 <125142449+Lassie111@users.noreply.github.com>
2024-03-31 00:19:09 +0200

    Update readme

--- build.sh
+++ build.sh
@@ -166,8 +166,8 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\n~~Install [TeamVanced/VancedMicroG](https://github.com/TeamVanced/VancedMicroG/releases) for non-root YouTube and YT Music~~"
-log "Install [ReVanced/GmsCore](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music"
+#log "\n~~Install [TeamVanced/VancedMicroG](https://github.com/TeamVanced/VancedMicroG/releases) for non-root YouTube and YT Music~~"
+log "\nInstall [ReVanced/GmsCore](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) module to detach YouTube and YT Music from Play Store (root)"
 log "\n[revanced-magisk-module](https://github.com/Lassie111/revanced-magisk-module)"
 log "\nChangelog:"
